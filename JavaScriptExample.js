d87b98cd6c221d72759c7f7942d8069cade7ee39
frederikhm <1637511+frederikhm@users.noreply.github.com>
2020-03-16 14:13:36 +0100

    Eksempel JavaScript kode

---
+++ JavaScriptExample.js
@@ -0,0 +1,34 @@
+//declare variable. start med lille, camelcasing
+let a = something; 
+
+//if, if else, else
+if (condition) {
+
+} else if (condition) {
+    
+} else {
+
+}
+
+//declare function. start med stort, camelcasing
+function FuncName(a, b, c) {
+
+}
+
+//arrowfunction call
+FuncName(a, b, (a, b, c) => {
+
+})
+
+//strings
+'normal string'
+
+`backtick for ${variable} string`
+
+//one line comment
+
+/*
+ * multi-
+ * line
+ * comment
+ */
