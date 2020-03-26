c927e49da767bfa019e7d4f1ba5458721b1058af
nannakmuller <nmall19@student.aau.dk>
2020-03-26 10:20:03 +0100

    nu virker input måske?

--- Node/PublicResources/inputOpPlan.js
+++ Node/PublicResources/inputOpPlan.js
@@ -2,4 +2,27 @@ function sendData(){
     let form = document.getElementById('opPlanForm');
     let formData = new FormData(document.getElementById('opPlanForm'));
     console.log(formData);
+    
+    let newOpPlan = {
+        coordinates: [document.getElementById('ncoordinate').value, document.getElementById('ecoordinate').value],
+            address:document.getElementById('address').value,
+            buildingDefinition:{
+                        Usage: document.getElementById('usage').value,
+                        height: document.getElementById('height').value,
+                        specialConsideration: document.getElementById('specialConsideration').value
+            },
+            firefightingEquipment:{
+                fireLift: document.getElementById('fireLift').value,
+                escapeStairs: document.getElementById('escapeStairs').value,
+                risers: document.getElementById('risers').value,
+                sprinkler: document.getElementById('sprinkler').value,
+                smokeDetectors: document.getElementById('smokeDetectors').value,
+                markers: document.getElementById('markers').value,
+                automaticFireDetector: document.getElementById('automaticFireDetector').value,
+                internalAlert: document.getElementById('internalAlert').value
+            },
+            consideration: document.getElementById('consideration').value,
+            //fullOpPlan:
+    }
+    console.log(newOpPlan);
 }
