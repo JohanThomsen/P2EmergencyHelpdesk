0f1a0a50614cd49c33a91dafc5fb1b724b189105
jthoms19 <jthoms19@student.aau.dk>
2020-03-26 10:28:18 +0100

    Form input virker

--- Node/PublicResources/inputOpPlan.js
+++ Node/PublicResources/inputOpPlan.js
@@ -9,19 +9,19 @@ function sendData(){
             buildingDefinition:{
                         Usage: document.getElementById('usage').value,
                         height: document.getElementById('height').value,
-                        specialConsideration: document.getElementById('specialConsideration').value
+                        specialConsideration: document.getElementById('specialConsiderations').value
             },
             firefightingEquipment:{
                 fireLift: document.getElementById('fireLift').value,
                 escapeStairs: document.getElementById('escapeStairs').value,
                 risers: document.getElementById('risers').value,
                 sprinkler: document.getElementById('sprinkler').value,
-                smokeDetectors: document.getElementById('smokeDetectors').value,
+                smokeDetectors: document.getElementById('smokeDetector').value,
                 markers: document.getElementById('markers').value,
                 automaticFireDetector: document.getElementById('automaticFireDetector').value,
                 internalAlert: document.getElementById('internalAlert').value
             },
-            consideration: document.getElementById('consideration').value,
+            consideration: document.getElementById('considerations').value,
             //fullOpPlan:
     }
     console.log(newOpPlan);
