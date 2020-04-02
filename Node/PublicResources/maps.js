90e7117710ada4e4b5f7e91dc7f99f72a34d70c1
Zzeeker <Rasmus.rbs@gmail.com>
2020-04-02 14:41:27 +0200

    Commented some stuff to prepare for opPlan overhaul

--- Node/PublicResources/maps.js
+++ Node/PublicResources/maps.js
@@ -78,7 +78,17 @@ function displayPlan(data){
 
     if (data) { // Checks whether the data arrived, if true, writes the information, otherwise displays an error message
         //Needs a full redesign, the properties layout does not fit the amount of data we need to display here, dropdowns are promising
-        for(property in data.opPlan){
+        
+        let p = document.createElement("p");
+        p.innerHTML = data.opPlan.address;
+        let attributeCount = Object.keys(data.opPlan).length + 1;
+        console.log(attributeCount-1);
+        let padding = ((outerElement.clientHeight / attributeCount) - 18) / 2; // that 18(text height) is really scuffed, figure out a change if necessary
+        p.style.margin = `${padding-1}px 2% ${padding-2}px 2%`; // -1 on both margin on account of padding, -1 on bottom because of border
+        p.style.padding = "1px";
+    
+        outerElement.appendChild(p);
+        /* for(property in data.opPlan){
             let p = document.createElement("p");
             p.innerHTML = data.opPlan[property];
             console.log(data.opPlan);
@@ -89,7 +99,7 @@ function displayPlan(data){
             p.style.padding = "1px";
     
             outerElement.appendChild(p);
-        }     
+        }   */   
 
     } else { // Styling could be improved, otherwise this section does its job
         let p = document.createElement("p");
