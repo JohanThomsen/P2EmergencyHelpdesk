ff02d671acf6565ee3f599d7582032aada28cc2c
Frederikhm <1637511+frederikhm@users.noreply.github.com>
2020-05-15 13:09:15 +0200

    no scroll fix

--- cypress/integration/UItest/uploadOpPlan.spec.js
+++ cypress/integration/UItest/uploadOpPlan.spec.js
@@ -2,14 +2,15 @@ describe('Opplan upload test', () => {
     const longitude = 9.932491
     const latitude = 57.04743
     it("Input coordinates", () => {
+        cy.viewport(1920,3000)
         cy.visit("http://127.0.0.1:3000/uploadOP")
 
         cy.get('#ncoordinate').should("be.empty")
-        cy.get('body').scrollTo('top')
+        // cy.get('body').scrollTo('top')
         cy.get('#ecoordinate').should("be.empty")
-        cy.get('body').scrollTo('top')
+        // cy.get('body').scrollTo('top')
         cy.get('#ncoordinate').type(longitude)
-        cy.get('body').scrollTo('top')
+        // cy.get('body').scrollTo('top')
         cy.get('#ecoordinate').type(latitude)
     })
 })
