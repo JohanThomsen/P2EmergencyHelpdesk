964456e0557f883f01f87ec31537806d5623a182
kristnaKris <56825435+kristnaKris@users.noreply.github.com>
2020-05-15 11:19:11 +0200

    Testing

---
+++ cypress/integration/UItest/uploadOpPlan.spec.js
@@ -0,0 +1,11 @@
+// describe('Opplan upload test', () => {
+//     it("Input coordinates", () =>{
+//         const longitude = 9.932491
+//         const latitude = 57.04743
+//         cy.visit("http://127.0.0.1:3000/uploadOP")
+//         cy.get('#ncoordinate').should(be.empty)
+//         cy.get('#ecoordinate').should(be.empty)
+//         cy.get('#ncoordinate').type(longitude)
+//         cy.get('#ecoordinate').type(latitude)
+//     })
+// })
