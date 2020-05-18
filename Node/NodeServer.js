f0802c032f524ae725ae9f4c1ddd94f061cbd7f3
jthoms19 <johan.thomsen@hotmail.com>
2020-05-18 10:47:50 +0200

    a

--- Node/NodeServer.js
+++ Node/NodeServer.js
@@ -170,7 +170,7 @@ function removeFireFromCommander(jsonData, response){
     response.end('\n')    
 }
 //server listen for requests 
-server.listen(port, hostName, () =>{
+server.listen(process.env.PORT || port, () =>{
 });
 console.log('server running');
 
